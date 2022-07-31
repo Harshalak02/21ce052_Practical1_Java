59dcb3745b88acce97de10fab709840303ddf25c
Harshalak02 <109841967+Harshalak02@users.noreply.github.com>
2022-07-31 18:31:32 +0530

    Update Second.java

--- Second.java
+++ Second.java
@@ -1,5 +1,5 @@
 // 21CE052 Harshal Karangale
-///*Aim-  Given a string, return a string made of the first 2 chars (if present), however include first  char  only  if  it  is  'o' and  include  the  second  only  if  it
+//Aim-  Given a string, return a string made of the first 2 chars (if present), however include first  char  only  if  it  is  'o' and  include  the  second  only  if  it
 //is  'z', so  "ozymandias" yields "oz",startOz("ozymandias") → "oz"startOz("bzoo") → "z"startOz("oxx") → "o"
 package Practical1;
 
